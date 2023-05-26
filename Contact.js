0f8af82f644425e0baa5ce91caf0b89b4856d018
codewithpatya <96979584+codewithpatya@users.noreply.github.com>
2023-05-26 23:47:37 +0530

    change in contact model

---
+++ Contact.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const contactSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    contact:{
+        type:Number,
+        required:true
+    },
+    message:String,
+    date: {
+        type: Date,
+        default:Date.now()
+    }
+})
+
+module.exports =  mongoose.model("Pcontact",contactSchema)
+
